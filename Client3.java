5be6f2f9fb4a4ae47e72216c2fe6e478781c3cef
Pranshul <pranshul.hatwar@gmail.com>
2022-11-05 16:40:32 +0530

    future ideas added in readme

--- Client3.java
+++ Client3.java
@@ -1,6 +1,6 @@
 public class Client3 {
     public static void main(String args[]) throws Exception{
-        java.net.Socket s = new java.net.Socket("localhost",3333);
+        java.net.Socket s = new java.net.Socket("192.168.1.6",3333);
         java.io.DataInputStream din = new java.io.DataInputStream(s.getInputStream());
         java.io.DataOutputStream dout = new java.io.DataOutputStream(s.getOutputStream());
         java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # socket_chat
+future inhancements
+1. Multiple Users 
+2. Each user name correponding to IP
+3. IP for Server
+4. Frontend Implementation - Swing or Android
+5. How to connect to mobile
+6. Easy file transfer
+7. Streaming audio and video at the same time
+8. Show all the IP's available in a network
